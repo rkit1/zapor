a0b84e3f68b41572daff7bbdc6c3ff69c3ed18cd
Victor Gorokhov <me@rkit.pp.ru>
2015-05-21 21:31:19 +0300

    Initial commit

---
+++ Arrows.h
@@ -0,0 +1,126 @@
+/* 
+ * File:   Arrows.h
+ * Author: victor
+ *
+ * Created on May 14, 2015, 8:57 PM
+ */
+
+#ifndef ARROWS_H
+#define	ARROWS_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+/* XPM */
+static char *arrow_small[]={
+"15 15 85 2",
+"Qt c #000000",
+".1 c #99431c",
+"#k c #9b461d",
+"#f c #a34d1f",
+".8 c #a85220",
+"#n c #ad5721",
+".T c #af5922",
+"#i c #b25c23",
+"#b c #b76124",
+"#r c #b96325",
+".g c #bb6c25",
+".J c #bc6626",
+".0 c #bc7b10",
+".2 c #c06d23",
+"#s c #c47521",
+".# c #c77928",
+".K c #c87629",
+"#l c #c87c1e",
+".z c #cf7e2a",
+"#g c #d08c1a",
+"#a c #d0990b",
+".n c #d2862b",
+"#q c #d6a30a",
+".f c #d88a2c",
+".9 c #d99b15",
+".U c #dda313",
+".S c #ddad08",
+".L c #e2962f",
+".h c #e39a30",
+"#h c #e4bb26",
+".A c #e59a30",
+"#o c #e5b20e",
+".a c #e99a31",
+"#j c #eec109",
+".b c #ef9832",
+".o c #ef9a32",
+".B c #ef9c32",
+".i c #ef9e32",
+".s c #efa032",
+".l c #efa232",
+".p c #efa432",
+".m c #efa733",
+".k c #f08731",
+".t c #f08931",
+".c c #f08b31",
+".j c #f08d31",
+".C c #f08f31",
+".e c #f09632",
+".d c #f18531",
+".D c #f1b745",
+".q c #f1bd4b",
+".u c #f2c251",
+".M c #f3cd5d",
+"#c c #f6d004",
+".E c #f6da5c",
+".v c #f6dc65",
+".r c #f7d441",
+".7 c #f8db24",
+"#m c #f8df51",
+".N c #f9e249",
+".3 c #fad802",
+".w c #fae646",
+".F c #fae84f",
+".O c #fde724",
+".G c #fdea2b",
+".x c #fee312",
+".y c #ffe000",
+".4 c #ffe206",
+".V c #ffe409",
+".P c #ffe710",
+"#d c #ffe813",
+".H c #ffea16",
+"#p c #ffec1d",
+".Z c #ffed29",
+"#e c #ffed6f",
+".I c #fff127",
+"#. c #fff22a",
+".R c #fff42d",
+".Q c #fff734",
+".W c #fff740",
+".X c #fffa80",
+"## c #fffbdf",
+".Y c #fffdcc",
+".5 c #fffee5",
+".6 c #ffffff",
+"QtQtQtQtQtQt.#.a.b.c.d.d.e.fQt",
+"QtQtQt.g.h.i.j.d.d.d.d.d.k.l.m",
+"Qt.n.m.o.c.d.d.d.d.d.k.e.p.q.r",
+".m.s.t.d.d.d.d.d.c.b.u.v.w.x.y",
+".z.A.B.C.j.C.B.D.E.F.G.H.I.I.y",
+".J.J.K.L.m.M.N.O.P.I.Q.Q.Q.R.S",
+".T.J.J.J.U.y.V.R.Q.Q.W.X.Y.Z.0",
+".1.J.J.J.2.3.4.Q.Q.Q.5.6.6.7Qt",
+"Qt.8.J.J.J.9.y#..Q.Q.6.6###aQt",
+"QtQt#b.J.J.J#c#d.Q.Q.6.6#eQtQt",
+"QtQt#f.J.J.J#g.y.R.Q.6.6#hQtQt",
+"QtQtQt#i.J.J.J#j#d.Q.5##.0QtQt",
+"QtQtQt#k.J.J.J#l.y.I.Y#mQtQtQt",
+"QtQtQtQt#n.J.J.J#o.V#p#qQtQtQt",
+"QtQtQtQtQtQt#r.J#s.3.yQtQtQtQt"};
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* ARROWS_H */
+
---
+++ main.c
@@ -0,0 +1,200 @@
+#define XUTIL_DEFINE_FUNCTIONS
+#include <X11/Xlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <X11/Xutil.h>
+#include <X11/extensions/XTest.h>
+#include <bmpfile.h>
+#include <X11/xpm.h>
+#include <string.h>
+#include "Arrows.h"
+#include <time.h>
+
+void dump_img(XImage* img) {
+    bmpfile_t* bmp = bmp_create(img->width, img->height, img->depth);
+    rgb_pixel_t px = {0, 0, 0, 0};
+    
+    for (int a = 0; a < img->width * img->height; a++) {
+        unsigned long spx = XGetPixel(img, a % img->width, a / img->width);
+        px.red = (spx & img->red_mask) >> (img->depth / 3 * 2);
+        px.green = (spx & img->green_mask) >> (img->depth / 3);
+        px.blue = spx & img->blue_mask;
+        bmp_set_pixel(bmp, a % img->width, a / img->width, px);
+    }
+  
+    bmp_save(bmp, "/home/victor/dump.bmp");
+    bmp_destroy(bmp);
+}
+
+void dump_xpm(Display* dis, XImage* img) {
+    printf("%d", XpmWriteFileFromImage(dis, "/home/victor/dump.xpm", img, NULL, NULL));
+}
+
+XImage* capture_screen(Display* dis) {
+    Window wind = XRootWindow(dis, XDefaultScreen(dis));
+    XWindowAttributes attrs;
+    XGetWindowAttributes(dis, wind, &attrs);
+    XImage* img = XGetImage(dis, wind, 0, 0, attrs.width, attrs.height
+            , AllPlanes, ZPixmap);
+    return img;
+}
+
+XImage* capture_screen_region(Display* dis, int x, int y, int x1, int y1) {
+    Window wind = XRootWindow(dis, XDefaultScreen(dis));
+    XImage* img = XGetImage(dis, wind, x, y, x1 - x, y1 - y, AllPlanes, ZPixmap);
+    return img;
+}
+
+void get_pointer(Display* dis, int* x, int* y) {
+    Window root_return, child_return;
+    int win_x_return, win_y_return;
+    unsigned int mask_return;
+    XQueryPointer(dis, XRootWindow(dis, XDefaultScreen(dis)), 
+            &root_return, &child_return, x, y, 
+            &win_x_return, &win_y_return, &mask_return);
+}
+
+void setup_listen_key(Display* dis, int key, unsigned long keyModifier) {
+    Window wind = XRootWindow(dis, XDefaultScreen(dis));
+    
+    XGrabKey(dis, key, keyModifier|Mod2Mask,
+            wind, True,
+            GrabModeAsync, GrabModeAsync);		
+    //scrolllock
+    XGrabKey(dis, key, keyModifier|Mod5Mask,
+            wind, True,
+            GrabModeAsync, GrabModeAsync);	
+    //capslock
+    XGrabKey(dis, key, keyModifier|LockMask,
+            wind, True,
+            GrabModeAsync, GrabModeAsync);
+
+    //capslock+numlock
+    XGrabKey(dis, key, keyModifier|LockMask|Mod2Mask,
+            wind, True,
+            GrabModeAsync, GrabModeAsync);
+
+    //capslock+scrolllock
+    XGrabKey(dis, key, keyModifier|LockMask|Mod5Mask,
+            wind, True,
+            GrabModeAsync, GrabModeAsync);						
+
+    //capslock+numlock+scrolllock
+    XGrabKey(dis, key, keyModifier|Mod2Mask|Mod5Mask|LockMask,
+            wind, True,
+            GrabModeAsync, GrabModeAsync);						
+
+    //numlock+scrollLock
+    XGrabKey(dis, key, keyModifier|Mod2Mask|Mod5Mask,
+            wind, True,
+            GrabModeAsync, GrabModeAsync);
+    
+    XSelectInput(dis, wind, KeyPressMask);
+}
+
+void capture_key_region(Display* dis) {
+    unsigned int f12 = XKeysymToKeycode(dis, XK_F12);
+    unsigned int f11 = XKeysymToKeycode(dis, XK_F11);
+    unsigned int f10 = XKeysymToKeycode(dis, XK_F10);
+    unsigned int f9 = XKeysymToKeycode(dis, XK_F9);
+    setup_listen_key(dis, f9, 0);
+    setup_listen_key(dis, f10, 0);
+    setup_listen_key(dis, f11, 0);
+    setup_listen_key(dis, f12, 0);
+    XEvent ev;
+    int x = 0, y = 0, x1 = 1, y1 = 1;
+    while(True) {
+        XNextEvent(dis, &ev);
+        
+        if (ev.type == KeyPress) {
+            printf("%x\n", ev.xkey.keycode);
+            if (ev.xkey.keycode == f10) break;
+            if (ev.xkey.keycode == f11) { 
+                get_pointer(dis, &x, &y);
+                printf("x:%d, y:%d\n", x, y);
+            }
+            if (ev.xkey.keycode == f12) {
+                get_pointer(dis, &x1, &y1);
+                printf("x1:%d, y1:%d\n", x1, y1);
+            }
+        }
+    }
+    dump_xpm(dis, capture_screen_region(dis, x, y, x1, y1));    
+}
+
+void click(Display* dis, int button, int x, int y) {
+    XTestFakeMotionEvent(dis, -1, x, y, 0);
+    XSync(dis,0);
+    usleep(30000);
+    XTestFakeButtonEvent(dis, button, True, 0);
+    XSync(dis,0);
+    usleep(30000);
+    XTestFakeButtonEvent(dis, button, False, 0);
+    XSync(dis,0);
+    usleep(30000);
+}
+
+bool check_for_image(XImage* haystack, int x, int y, XImage* needle) {
+    for (int ty = 0; ty < needle->height; ty++) {
+        for (int tx = 0; tx < needle->width; tx++) {
+            unsigned long tpx = XGetPixel(needle, tx, ty);
+            if (tpx > 0) {
+                if (tpx != XGetPixel(haystack, tx + x, ty + y)) {
+                    return False;
+                }
+            }
+        }
+    }
+    return True;
+}
+
+int image_loop(Display* dis) {
+    XImage* img;
+    XImage* imgcur;
+    XImage* tri;
+    bool exit_error = False;
+    XpmCreateImageFromData(dis, arrow_small, &tri, NULL, NULL);
+    img = capture_screen(dis);
+    
+    for (int iy = 0; iy < img->height - tri->height && !exit_error; iy++) {
+        for (int ix = 0; ix < img->width - tri->width && !exit_error; ix++) {
+            
+            if (check_for_image(img, ix, iy, tri)) {
+                imgcur = capture_screen_region(dis, ix, iy, ix + tri->width, iy + tri->height);
+                if(!check_for_image(imgcur, 0, 0, tri)) {
+                    exit_error = True;
+                } else {
+                    printf("%d %d\n", ix, iy);
+                    click(dis, 1, ix + 5, iy + 5);
+                }
+                XFree(imgcur);
+            }
+            
+        }
+    }
+    
+    XFree(img);
+    XFree(tri);
+    if (exit_error) return -1;
+    return 0;
+}
+
+int main(int argc, char** argv) {
+    Display* dis = XOpenDisplay(0);
+    unsigned long f9 = XKeysymToKeycode(dis, XK_F9);
+    setup_listen_key(dis, f9, ControlMask);
+    XEvent ev;
+    while(True) {
+        XNextEvent(dis, &ev);
+        if (ev.type == KeyPress) {
+            if (ev.xkey.keycode == f9) {
+                if (image_loop(dis) == -1) 
+                    printf("exited with error\n");
+            }
+        }
+    }
+    XFree(dis);
+    return (EXIT_SUCCESS);
+}
+
